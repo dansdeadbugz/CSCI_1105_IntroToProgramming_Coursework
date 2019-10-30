645b6b66e8739cc24b13d2f5fef759a2df522989
dansdeadbugz <57117335+dansdeadbugz@users.noreply.github.com>
2019-10-30 16:46:19 -0600

    Update and rename Excercise1_5.java to Exercise1_5.java

--- Chapter 1/Exercise1_5.java
+++ Chapter 1/Exercise1_5.java
@@ -4,7 +4,7 @@
 	Write a program that does some math
 */
 
-public class Excercise1_5{
+public class Exercise1_5{
 	public static void main(String[] args) {
 		System.out.println((9.5*4.5-2.5*3)/(45.5-3.5));				
 	}
