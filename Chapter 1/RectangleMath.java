e4fbb3391fb971a230f7505b608fd741ba738201
dansdeadbugz <57117335+dansdeadbugz@users.noreply.github.com>
2019-10-30 16:44:07 -0600

    Add files via upload

---
+++ Chapter 1/RectangleMath.java
@@ -0,0 +1,13 @@
+//	Daniel Morris
+//	October 30, 2019
+//	Write a program to display the area and perimeter of a rectangle
+
+class RectangleMath {
+	public static void main(String[] args) {
+		System.out.println("Area and Perimeter of Rectangle measuring 4.5 x 7.9");
+		System.out.println("Area =");
+		System.out.println(4.5 * 7.9);
+		System.out.println("Perimeter =");
+		System.out.println(4.5 + 4.5 + 7.9 + 7.9);
+	}
+}
