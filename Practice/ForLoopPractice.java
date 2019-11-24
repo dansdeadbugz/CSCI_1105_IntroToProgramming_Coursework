2ca18658f3d08820cd3b03b7c1168ad844f3539b
dansdeadbugz <57117335+dansdeadbugz@users.noreply.github.com>
2019-11-24 13:10:15 -0700

    Add files via upload

---
+++ Practice/ForLoopPractice.java
@@ -0,0 +1,10 @@
+class ForLoopPractice {
+	public static void main(String[] args) {
+		for(int i = 0; i < 6; i++){
+			for(int j = 0; j < 4; j++){
+				System.out.println("i = " + i + " j = " + j);
+			}
+			
+		}
+	}
+}
