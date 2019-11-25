889b4331865d2080022e549a465024539a2dde6d
dansdeadbugz <57117335+dansdeadbugz@users.noreply.github.com>
2019-11-25 16:00:25 -0700

    Add files via upload

---
+++ Practice/Exercise6_3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Exercise6_3 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in)
+		
+		System.out.print("Enter an Integer:  ");
+		int number = input.nextInt();
+		if (isPalindrome){
+			System.out.println(number + " is a palindrome!");
+		else 
+			System.out.println(number " is not a palindrome.");
+		}
+		
+		
+	{	
+	public static boolean isPalindrome(int number){
+		boolean isPalendrome = true;
+		int backwards = reverse(number); 
+		if (backwards = number)
+			return isPalendrome = true;
+		else 
+			return isPalendrome = false; 
+		
+	}	
+	public static int reverse(int number){
+		while (number != 0)
+		
+	}
+	
+	}
+}
